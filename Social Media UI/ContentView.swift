58ae49d633715eb498589d23ede40eb4487b7de6
sirx2713 <bradx2713@gmail.com>
2024-03-15 21:07:49 +0300

    Initial Commit

---
+++ Social Media UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Social Media UI
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Social Media UI/Social_Media_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_Media_UIApp.swift
+//  Social Media UI
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct Social_Media_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
